6db11b14a08fcf8518ef698c6046bc3dbab2ca9b
Daniel Pimenta <d.pimenta@campus.fct.unl.pt>
2017-11-17 12:04:54 +0000

    Added lock free implementation.

--- cp_proj2/src/cp/benchmark/intset/IntSetLinkedListLockFree.java
+++ cp_proj2/src/cp/benchmark/intset/IntSetLinkedListLockFree.java
@@ -1,5 +1,7 @@
 package cp.benchmark.intset;
 
+import java.util.concurrent.atomic.AtomicMarkableReference;
+
 /**
  * @author Pascal Felber
  * @author Tiago Vale
@@ -7,104 +9,116 @@ package cp.benchmark.intset;
  */
 public class IntSetLinkedListLockFree implements IntSet {
 
-  public class Node {
-    private final int m_value;
-    private Node m_next;
+	public class Node {
 
-    public Node(int value, Node next) {
-      m_value = value;
-      m_next = next;
-    }
+		private final int value;
+		private AtomicMarkableReference<Node> next;
 
-    public Node(int value) {
-      this(value, null);
-    }
+		public Node(int value, Node next) {
+			this.value = value;
+			this.next = new AtomicMarkableReference<Node>(next, false);
+		}
 
-    public int getValue() {
-      return m_value;
-    }
+		public Node(int value) {
+			this(value, null);
+		}
 
-    public void setNext(Node next) {
-      m_next = next;
-    }
+		public int getValue() {
+			return value;
+		}
+		
+		public boolean compareAndSetNext(Node expectedNode, Node newNode, boolean expectedMark, boolean newMark) {
+			return this.next.compareAndSet(expectedNode, newNode, expectedMark, newMark);
+		}
+		
+		public boolean attemptMarkNext(Node expectedNode, boolean newMark) {
+			return this.next.attemptMark(expectedNode, newMark);
+		}
 
-    public Node getNext() {
-      return m_next;
-    }
-  }
+		public AtomicMarkableReference<Node> getNext() {
+			return next;
+		}
 
-  private final Node m_first;
+	}
 
-  public IntSetLinkedListLockFree() {
-    Node min = new Node(Integer.MIN_VALUE);
-    Node max = new Node(Integer.MAX_VALUE);
-    min.setNext(max);
-    m_first = min;
-  }
+	private final Node m_first;
 
-  public boolean add(int value) {
-    boolean result;
+	public IntSetLinkedListLockFree() {
+		m_first = new Node(Integer.MIN_VALUE, new Node(Integer.MAX_VALUE));
+	}
 
-    Node previous = m_first;
-    Node next = previous.getNext();
-    int v;
-    while ((v = next.getValue()) < value) {
-      previous = next;
-      next = previous.getNext();
-    }
-    result = v != value;
-    if (result) {
-      previous.setNext(new Node(value, next));
-    }
+	public Node find(int value) {
+		retry: while (true) {
+			Node previous = m_first;
+			Node current = previous.getNext().getReference();
+			while (true) {
+				boolean[] marked = { false };
+				Node next = current.getNext().get(marked);
+				while (marked[0]) {
+					if (!previous.compareAndSetNext(current, next, false, false)) 
+						continue retry;
+					current = next;
+					next = current.getNext().get(marked);
+				}
+				if (current.getValue() >= value)
+					return previous;
+				previous = current;
+				current = next;
+			}
+		}
+	}
 
-    return result;
-  }
 
-  public boolean remove(int value) {
-    boolean result;
+	public boolean add(int value) {
+		while (true) {
+			Node previous = this.find(value);
+			Node current = previous.getNext().getReference();
+			if (current.getValue() == value) {
+				return false;
+			}
+			if (previous.compareAndSetNext(current, new Node(value, current), false, false)) {
+				return true;
+			}
+		}
+	}
 
-    Node previous = m_first;
-    Node next = previous.getNext();
-    int v;
-    while ((v = next.getValue()) < value) {
-      previous = next;
-      next = previous.getNext();
-    }
-    result = v == value;
-    if (result) {
-      previous.setNext(next.getNext());
-    }
+	public boolean remove(int value) {
+		while (true) {
+			Node previous = this.find(value);
+			Node current = previous.getNext().getReference();
+			if (current.getValue() != value) {
+				return false;
+			}
+			Node next = current.getNext().getReference();
+			if (current.attemptMarkNext(next, true)) {
+				previous.compareAndSetNext(current, next, false, false);
+				return true;
+			}
+		}
+	}
 
-    return result;
-  }
+	public boolean contains(int value) {
+		Node current = m_first;
+		while (current.getValue() < value) {
+			current = current.getNext().getReference();
+		}
+		boolean[] marked = { false };
+		current.getNext().get(marked);
+		return current.getValue() == value && !marked[0];
+	}
 
-  public boolean contains(int value) {
-    boolean result;
-
-    Node previous = m_first;
-    Node next = previous.getNext();
-    int v;
-    while ((v = next.getValue()) < value) {
-      previous = next;
-      next = previous.getNext();
-    }
-    result = (v == value);
-
-    return result;
-  }
-
-  public void validate() {
-    java.util.Set<Integer> checker = new java.util.HashSet<>();
-    int previous_value = m_first.getValue();
-    Node node = m_first.getNext();
-    int value = node.getValue();
-    while (value < Integer.MAX_VALUE) {
-      assert previous_value < value : "list is unordered: " + previous_value + " before " + value;
-      assert !checker.contains(value) : "list has duplicates: " + value;
-      checker.add(value);
-      previous_value = value;
-      node = node.getNext();
-      value = node.getValue();
-    }
-  }
+	public void validate() {
+		java.util.Set<Integer> checker = new java.util.HashSet<>();
+		int previous_value = m_first.getValue();
+		Node node = m_first.getNext().getReference();
+		int value = node.getValue();
+		while (value < Integer.MAX_VALUE) {
+			assert previous_value < value : "list is unordered: " + previous_value + " before " + value;
+			assert !checker.contains(value) : "list has duplicates: " + value;
+			checker.add(value);
+			previous_value = value;
+			node = node.getNext().getReference();
+			value = node.getValue();
+		}
+	}
 }
